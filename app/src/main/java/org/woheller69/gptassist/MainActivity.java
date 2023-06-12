c2fa53556e3b01203caa456a89b7b22760b0e134
woheller69 <wolfgang.heller@web.de>
2023-06-12 07:21:26 +0200

    fix login issues on some devices #2

--- app/src/main/java/org/woheller69/gptassist/MainActivity.java
+++ app/src/main/java/org/woheller69/gptassist/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onPause() {
+        if (chatCookieManager!=null) chatCookieManager.flush();
         super.onPause();
     }
 
